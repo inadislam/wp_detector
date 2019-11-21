f920456d4231102267a8ae591279c8bb43229927
inadislam <48486098+inadislam@users.noreply.github.com>
2019-11-21 14:03:43 +0600

    Wp Detector
    Done

---
+++ wp_splitter.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+o = open ("others.txt", "a+")
+wp = open ("wp.txt", 'a+')
+wl = open ("list.txt").read().split("\n")
+
+class tcolor:
+    yellow = '\33[33m'
+    red = '\33[31m'
+    green = '\33[32m'
+
+def splitter(host):
+        host = host
+        print (tcolor.green+"[START] "+tcolor.yellow+"Scanner Started Successfully")
+        
+        try:
+            test_response = requests.get(host+'/').text
+        except requests.exceptions.HTTPError:
+            o.write(host+'#notWP\n')
+            print (tcolor.red+"[ERROR] "+tcolor.yellow+"Connection Error")
+            pass
+        except requests.exceptions.ConnectionError:
+            o.write(host+'#notWP\n')
+            print(tcolor.red+"[ERROR] "+tcolor.yellow+"Connection Error")
+            pass
+        else:
+            if "wp-content" in test_response:
+                wp.write(host+'\n')
+                print(tcolor.yellow+"[OK] "+tcolor.green+"It is a Wordpress Site")
+            else:
+                o.write(host+'#NotWP\n')
+                print(tcolor.yellow+"[ERROR] "+tcolor.red+"It is not a Wordpress Site")
+
+def banner():
+    os.system("clear")
+    
+    print("###########################################################")
+    print("#        Tool: WordPress Site splitter                    #")
+    print("#        Author: Inad Islam                               #")
+    print("#   A Wordpress site splitter for Private User's Only     #")
+    print("###########################################################")
+
+def s():
+    banner()
+    for item in wl:
+        if (item == ""):
+            continue
+        splitter(item)
+
+s()
